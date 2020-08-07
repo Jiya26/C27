2376cf17142cf25d5eaee41737aa040ec95f160f
jiya <reshma.singhvi@gmail.com>
2020-08-07 11:12:00 +0530

    created constrant

--- sketch.js
+++ sketch.js
@@ -1,10 +1,13 @@
 const Engine = Matter.Engine;
 const World= Matter.World;
 const Bodies = Matter.Bodies;
+const Constraint =Matter.Constraint;
 
 var engine, world;
 var box1, pig1;
 var backgroundImg,platform;
+var Clog;
+var chain;
 
 function preload() {
     backgroundImg = loadImage("sprites/bg.png");
@@ -35,6 +38,8 @@ function setup(){
     log5 = new Log(870,120,150, -PI/7);
 
     bird = new Bird(100,100);
+    Clog = new Log(100,150,100,PI/5);
+    chain = new Chain(bird.body,Clog.body)
 
 }
 
@@ -61,4 +66,6 @@ function draw(){
 
     bird.display();
     platform.display();
+    Clog.display();
+    chain.display();
 }
